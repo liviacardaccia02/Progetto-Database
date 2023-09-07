7c57b5e6d736d9ed23faaafa8fff221a43577fc2
Livia Cardaccia <livia.cardaccia@studio.unibo.it>
2023-09-07 17:30:55 +0200

    aggiunte e corrette tutte le operazioni

--- sql/Query.sql
+++ sql/Query.sql
@@ -28,26 +28,29 @@ use SocialNetworkPalestra;
 --VALUES (?, ?, ?)
 
 -- OP 6
---SELECT E.Nome
---FROM Esercizio E JOIN Aggiunta A ON (E.Nome = A.NomeEsercizio) AND (E.GruppoMuscolare = A.GruppoMuscolareEsercizio)
---WHERE A.CodiceAllenamento = 7
+--SELECT AL.Titolo AS 'Allenamento', E.Nome AS 'Nome esercizio', E.GruppoMuscolare
+--FROM AGGIUNTA A
+--FULL JOIN ESERCIZIO E ON (E.Nome = A.NomeEsercizio) AND (E.GruppoMuscolare = A.GruppoMuscolareEsercizio)
+--LEFT JOIN ALLENAMENTO AL ON (A.CodiceAllenamento = AL.CodiceAllenamento)
+--WHERE A.CodiceAllenamento = 9
  
 -- OP 7
---INSERT INTO POST (AllenamentoCollegato, DataPubblicazione, Titolo, Didascalia, Autore)
+--INSERT INTO POST (Interazione, AllenamentoCollegato, DataPubblicazione, Titolo, Didascalia, Autore)
 --VALUES (?, ?, ?, ?, ?)
 
 -- OP 8
---SELECT U.Nome, U.Cognome, U.Username, P.Titolo as 'Titoli post pubblicati',
+--SELECT U.Nome, U.Cognome, U.Username, P.Titolo as 'Post pubblicato',
 --P.DataPubblicazione as 'Data di pubblicazione'
 --FROM UTENTE U
 --LEFT JOIN POST P ON U.IDutente = P.Autore
 --WHERE U.IDutente = 1
 
 -- OP 9
---SELECT U.Username
---FROM UTENTE U
---LEFT JOIN AMICIZIA A ON U.IDutente = A.Ricevente
---WHERE U.IDutente = 1
+--SELECT U1.Username, U2.Username as 'Follower', A.StatoAmicizia
+--FROM UTENTE U1
+--LEFT JOIN AMICIZIA A ON U1.IDutente = A.Ricevente
+--FULL JOIN  UTENTE U2 ON A.Richiedente = U2.IDutente
+--WHERE U1.IDutente = 1
 
 -- OP 10
 --SELECT Nome, Cognome, Specializzazione
