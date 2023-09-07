6462aaaeeec60c0d1fe9cedc64da1599c696691f
Livia Cardaccia <livia.cardaccia@studio.unibo.it>
2023-09-07 15:39:29 +0200

    aggiunte nuove query

--- sql/Query.sql
+++ sql/Query.sql
@@ -2,38 +2,61 @@ use SocialNetworkPalestra;
 
 -- OP 1
 --INSERT INTO UTENTE (Nome, Cognome, Username, Email, DataRegistrazione, NumeroTelefono)
---VALUES (?, ?, ?, ?, ?, ?)-- OP 2--INSERT INTO MISURAZIONE_CORPOREA (Utente, DataMisurazione, Peso, Altezza, CirconferenzaVita, CirconferenzaBraccia, CirconferenzaGambe)
---VALUES (?, ?, ?, ?, ?, ?, ?)-- OP 3--INSERT INTO ESERCIZIO (Nome, GruppoMuscolare, Descrizione, Creatore)
+--VALUES (?, ?, ?, ?, ?, ?)
+
+-- OP 2
+--INSERT INTO MISURAZIONE_CORPOREA (Utente, DataMisurazione, Peso, Altezza,
+-- CirconferenzaVita, CirconferenzaBraccia, CirconferenzaGambe)
+--VALUES (?, ?, ?, ?, ?, ?, ?)
+
+-- OP 3
+--INSERT INTO ESERCIZIO (Nome, GruppoMuscolare, Descrizione, Creatore)
 --VALUES (?, ?, ?, ?)
 
 -- OP 4
---SELECT Nome as 'Nome esercizio', GruppoMuscolare as 'Muscoli coinvolti', Descrizione
---FROM ESERCIZIO
+--SELECT E.Nome as 'Esercizio', E.GruppoMuscolare as 'Gruppo muscolare coinvolto',
+--E.Descrizione, U.Username as 'Creatore'
+--FROM ESERCIZIO E, UTENTE U
+--WHERE E.Creatore = U.IDutente
+
 
 -- OP 5
 --INSERT INTO ALLENAMENTO (Titolo, Data, Durata, LivelloIntesità, Utente)
---VALUES (?, ?, ?, ?, ?)--INSERT INTO AGGIUNTA (NomeEsercizio, GruppoMuscolareEsercizio, CodiceAllenamento)
---VALUES (?, ?, ?)-- OP 6--INSERT INTO AGGIUNTA (NomeEsercizio, GruppoMuscolareEsercizio, CodiceAllenamento)
---VALUES (?, ?, ?) -- OP 7--INSERT INTO POST (AllenamentoCollegato, DataPubblicazione, Titolo, Didascalia, Autore)
---VALUES (?, ?, ?, ?, ?)-- OP 8--INSERT INTO INTERAZIONE (TipoInterazione, Testo, Data, Utente)
---VALUES (’C’, ?, ?, ?)-- OP 9--INSERT INTO INTERAZIONE (TipoInterazione, Testo, Data, Utente)
---VALUES (’L’, ?, ?, ?)-- OP 10--INSERT INTO AMICIZIA (Richiedente, Ricevente, StatoAmicizia)
---VALUES (?, ?, ’In attesa’)
+--VALUES (?, ?, ?, ?, ?)
+
+--INSERT INTO AGGIUNTA (NomeEsercizio, GruppoMuscolareEsercizio, CodiceAllenamento)
+--VALUES (?, ?, ?)
+
+-- OP 6
+--SELECT E.Nome
+--FROM Esercizio E JOIN Aggiunta A ON (E.Nome = A.NomeEsercizio) AND (E.GruppoMuscolare = A.GruppoMuscolareEsercizio)
+--WHERE A.CodiceAllenamento = 7
+ 
+-- OP 7
+--INSERT INTO POST (AllenamentoCollegato, DataPubblicazione, Titolo, Didascalia, Autore)
+--VALUES (?, ?, ?, ?, ?)
+
+-- OP 8
+--SELECT U.Nome, U.Cognome, U.Username, P.Titolo as 'Titoli post pubblicati',
+--P.DataPubblicazione as 'Data di pubblicazione'
+--FROM UTENTE U
+--LEFT JOIN POST P ON U.IDutente = P.Autore
+--WHERE U.IDutente = 1
+
+-- OP 9
+--SELECT U.Username
+--FROM UTENTE U
+--LEFT JOIN AMICIZIA A ON U.IDutente = A.Ricevente
+--WHERE U.IDutente = 1
+
+-- OP 10
+--SELECT Nome, Cognome, Specializzazione
+--FROM MEDICO
 
 -- OP 11
---UPDATE AMICIZIA
---SET StatoAmicizia = ?
---WHERE Richiedente = ? AND Ricevente = ?
-
--- OP 12
 --INSERT INTO MEDICO (CF, Nome, Cognome, Email, NumeroTelefono, Specializzazione)
 --VALUES (?, ?, ?, ?, ?, ?)
 
--- OP 13
+-- OP 12
 --INSERT INTO VISITA (Data, Ora, Prezzo, Paziente)
 --VALUES (?, ?, ?, ?)
-
--- OP 14
---UPDATE VISITA
---SET Medico = ?
---WHERE CodiceVisita = ?
